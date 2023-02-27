1043c1d19af5293545fa7c104e04b363d4fa01ee
thesnake94 <dromdhane@guardiaschool.fr>
2023-02-27 12:31:41 +0100

    compter le nombre de caractère et l'afficher

--- put.c
+++ put.c
@@ -1,16 +1,25 @@
 #include <unistd.h>
+#include <stdio.h>
+
 
 void gd_putchar(char c) {
     write(1, &c, 1);
 }
 
 int gd_putstr(char *str){
-   
+    int i = 0;
+
+    while (str[i] != '\0'){
+        gd_putchar(str[i]);
+        i++;
+
+    }
+    return(i);
+    
 }
 
 int main(void) {
-    // gd_putchar('a');
-    // gd_putchar('\n');
     int nb_put = gd_putstr("Coucou");
-
+    printf("%i\n", nb_put);
+    return(1);
 }
