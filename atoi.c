da5a98fb4f819fc07a79696daafa6bca2722c404
thesnake94 <dromdhane@guardiaschool.fr>
2023-02-28 12:01:38 +0100

    modif

--- atoi.c
+++ atoi.c
@@ -1,18 +1,46 @@
-void gd_putnbr(int nb){
+#include <stdio.h>
 
-}
+// void gd_putnbr(int nb){
+// }
+// void gd_putchar(char c){
+//     write(1 , &c, 1);
+// }
 
-void gd_putchar(char c){
-    write(1 , &c, 1);
-}
+void gd_putnbr (int nb);
+void gd_putchar (char c);
+
 
 int gd_atoi(char *str){
+    int i = 0;
+    int ret = 0;
+    int neg = 1;
+    int nb;
+
+    while(str[i]){
+        if ((str[i] >= 48 && str[i] <=59) || (str[i] == 43 || str[i] == 45)) // entre 48 et 59 dans la table ascii pour les chiffres
+            break;
+        i++;
+    }
+    if (str[i] == 43 || str[i] == 45){ //dans la tbables ascii pour le plus et le moins
+        if (str[i] == 45)
+            neg = -1;
+        i++;
+    } 
+
+    while(str[i]) {
+        nb = str[i] - 48;
+        ret = ret*10 + nb;
+        i++;
+    }
+
+    return(ret*neg);
 
 }
 
 int main(void){
-    int nb = gd_atoi("-500");
-    gd_putnbr(nb);
-    gd_putchar('\n');
-    
+    int nb = gd_atoi("-51234");
+    // gd_putnbr(nb);
+    // gd_putchar('\n');
+    printf("%i\n", nb);
+
 }
