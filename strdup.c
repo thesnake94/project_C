21d216ec2184a73bef9552a41c790786a514e92b
thesnake94 <dromdhane@guardiaschool.fr>
2023-03-01 10:11:45 +0100

    strdup

---
+++ strdup.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int gd_strlen(const char *str){
+    int len = 0;
+    
+    while(str[len])
+        len++;       
+    return(len);
+}
+
+char *gd_strdup(const char *src){
+    int len_src = gd_strlen(src);
+    char *ret;
+
+    len_src = gd_strlen(src);
+    ret = (char *)malloc(sizeof(char) * (len_src + 1));
+    if (ret == NULL)
+        return NULL;
+
+
+    len_src = 0;
+    while(src[len_src]){
+        ret[len_src] = src[len_src];
+        len_src++;
+    }
+
+    ret[len_src] = '\0';
+    return(ret);
+}
+
+int main(void){
+    char *str = "zoa";
+    char *dup;
+
+    dup = gd_strdup(str);
+    printf("%s\n", dup);
+    free(dup);
+}
