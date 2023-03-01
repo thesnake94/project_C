14013ea0e2577dd27549c97324bbaa4525aaa4dc
thesnake94 <dromdhane@guardiaschool.fr>
2023-03-01 17:51:25 +0100

    lst

--- lst.c
+++ lst.c
@@ -1,12 +1,35 @@
+// #include "includes.h"
+
+// int main(void) {
+//     int array_int[5] = {5,6,4,2,1};
+//     int i = 0;
+//     while (i < 5) {
+//         printf
+//         ("%i\n", array_int[i]);
+//         i++;
+//     }
+    
+// }
+
 #include "includes.h"
 
+int compare_ints(const void *a, const void *b) {
+    const int *pa = (const int *) a;
+    const int *pb = (const int *) b;
+    return (*pa > *pb) - (*pa < *pb);
+}
+
 int main(void) {
-    int array_int[5] = {5,6,4,2,1};
+    int array_int[5] = {5, 6, 4, 2, 1};
     int i = 0;
+
+    qsort(array_int, 5, sizeof(int), compare_ints);
+
+    i = 0;
     while (i < 5) {
-        printf
-        ("%i\n", array_int[i]);
+        printf("%i\n", array_int[i]);
         i++;
     }
-    
+
+    return 0;
 }
