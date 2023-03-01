1419e9eb065a6dbba3b91932a15a40e233752327
thesnake94 <dromdhane@guardiaschool.fr>
2023-03-01 17:52:10 +0100

    modif lst

--- lst.c
+++ lst.c
@@ -1,15 +1,5 @@
-// #include "includes.h"
 
-// int main(void) {
-//     int array_int[5] = {5,6,4,2,1};
-//     int i = 0;
-//     while (i < 5) {
-//         printf
-//         ("%i\n", array_int[i]);
-//         i++;
-//     }
-    
-// }
+// trier ce tableau avec qsort + créer des listes chainées
 
 #include "includes.h"
 
