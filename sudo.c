f607aa86097bf425647d0e7cbd366b090c425a06
thesnake94 <dromdhane@guardischool.fr>
2023-03-08 12:57:58 +0100

    SUDO

---
+++ sudo.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+    char buf[90];
+    int i = 0;
+    int **board;
+
+    int fd = open(argv[1], O_RDONLY);
+    int size = read(fd, buf, 90);
+
+    // allouer la mémoire du int ** -> 9lignes (9 int *)
+    board = (int **)malloc(9 * sizeof(int *));
+
+    // loop *9 allouer la mémoire des int* -> (9 int)
+    while (i < 9)
+    {
+        board[i] = (int *)malloc(9 * sizeof(int));
+        i++;
+    }
+
+    printf("%s\n", buf);
+    printf("%i\n", size);
+    buf[size] = '\0';
+    close(fd);
+
+    // libere mémoire
+    i = 0;
+    while (i < 9)
+    {
+        free(board[i]);
+        i++;
+    }
+    free(board);
+}
